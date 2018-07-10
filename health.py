55dfc1f7e927c38fa8eca2abeeddaafaf73987fb
KurachAV <kurachav@mosreg.ru>
2018-07-10 17:26:15 +0300

    add health

---
+++ health.py
@@ -0,0 +1,7 @@
+'''
+@since: 10 July 2018
+@author: Kurach Aleksandr <sundelor@gmail.com>
+@license: GPL
+@summary: Warranty check for PyLXCA
+'''
+import
--- warranty.py
+++ warranty.py
@@ -15,12 +15,17 @@ r = requests.get(ip+url_warranty, auth=HTTPBasicAuth(user, passwd), verify=False
 d = json.loads(r.text)
 
 for item in d:
-    print("chassis: " + item['deviceName']) # get chasis
+    print("Chassis Name: " + item['deviceName']) # get chasiss
+    print("Status: " + item['status'])
+    print("End Date: " + item['endDate'])
+    print()
     tr = objectpath.Tree(item['children'])
     t2 = tuple(tr.execute('$..children'))
     for device in t2:
         if 'deviceType' in device:
+            print("Node Name: " + device['deviceName'])
             print("sn: " + device['serialNumber'])
-            print("End Date: " + device['endDate'])
             print("Status: " + device['status'])
-            
+            print("End Date: " + device['endDate'])
+            print()
+
